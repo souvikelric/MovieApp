92219f3ae387a9f68a407c6d7bec0f09284356b9
Souvik Roy Chowdhury <souvikelric@gmail.com>
2024-02-13 20:20:20 +0530

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+import "./App.css";
 
-function App() {
+// 7aac7ca0
+
+// const movie1 = {
+//   Title: "Batman v Superman: Dawn of Justice",
+//   Year: "2016",
+//   imdbID: "tt2975590",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+// };
+
+export default function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const movieSearch = async (title) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=7aac7ca0&s=${title}`
+    );
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    movieSearch("Superman");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              movieSearch(searchTerm);
+            }
+          }}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search Icon"
+          onClick={() => {
+            movieSearch(searchTerm);
+          }}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie1) => {
+            return <MovieCard key={movie1.Title} movie1={movie1} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+export default function MovieCard({movie1}) {
+  return (
+    <div className="movie">
+    <div>
+      <p>{movie1.Year}</p>
+    </div>
+    <div>
+      <img
+        src={
+          movie1.Poster !== "N/A"
+            ? movie1.Poster
+            : "https://via.placeholder.com/400"
+        }
+        alt="poster"
+      />
+    </div>
+    <div>
+      <span>{movie1.Type}</span>
+      <h3>{movie1.Title}</h3>
+    </div>
+  </div>
+  )
+}
